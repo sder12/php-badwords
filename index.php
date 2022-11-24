40986d9c876ddcd1c893d2dbc28e739dcdc0604f
sder12 <sara.cetto@gmail.com>
2022-11-24 16:36:00 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!-- Creare un form PHP che permetta di inviare due campi: un paragrafo ed una parola da censurare.
+Gestire il tutto con due file diversi.
+Il primo file dovrà permettere all’utente di inserire i dati e inviare la richiesta al server.
+Il secondo file riceverà la richiesta ed eseguirà queste operazioni:
+- stampare a schermo il paragrafo e la sua lunghezza
+- stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>badwords</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <!-- TITLE -->
+        <div class="title">
+            <h1>FORM PHP</h1>
+            <span>
+                <?php echo "php debug subtitle"?>
+            </span>
+        </div>
+
+
+        <!-- FORM -->
+
+    </div>
+</body>
+</html>
